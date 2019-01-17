95e1a35a00d2df4b7e569afa9e43341f069f6142
adwait1-g <adwait.gautham@gmail.com>
2019-01-17 18:39:42 +0530

    Added a few testcases

---
+++ test/Lexical Analyser/test11.c
@@ -0,0 +1,17 @@
+int func_name(int arg0, float arg1, char arg2);
+
+
+
+int main() {
+
+	func_name(2, 3.3, 'a');
+	return 0;
+}
+
+
+int func_name(int arg0, float arg1, char arg2) {
+
+	printf("A statement!");
+	return -1;
+}
+
