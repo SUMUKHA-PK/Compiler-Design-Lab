e5200d1e0619334b712856d6ef2c8182b97f450c
Naladala Indukala <15co230.indukala@nitk.edu.in>
2019-03-08 11:41:45 +0530

    Added main1.l, main1.y with correct grammars

--- src/Parser/tableStructure.h
+++ src/Parser/tableStructure.h
@@ -1,5 +1,45 @@
-#include "../Lexical Analyser/symbolTable.h"
+#ifndef _TABLE_STRUCTURE_H
+#define _TABLE_STRUCTURE_H 1
 
-typedef struct node{
+#include<stdio.h>
+    #include<stdlib.h>
+    #include<string.h>
+
+int symbolTableSize = 0,constantTableSize=0;
     
-}Node
+// Hardcoded TableSize value. 0
+int TableSize = 1000;
+
+
+// Structure of a symbol
+typedef struct symbol {
+	char value[100];
+	char type[100];
+	int line;
+	struct symbol* next; 
+} symbolToken;
+
+// 2 Tables are defined: symbolTable and constantTable
+symbolToken * symbolTable[1000];
+symbolToken * constantTable[1000];
+
+// Initializes the 2 tables - allocates memory to 2 tables.
+void initTables();
+
+// Print a give table
+// table = 0 => print symbolTable
+// table = 1 => print constantTable
+void printTable(int table);
+
+
+// The awesome hash function
+int hash(unsigned char * s);
+
+// Creates, initialises a symbol and returns a pointer to it.
+symbolToken* createsymbolToken(char *value, char *type, int lineNumber);
+
+// Inserts a given symbol into a specified table.
+void insertsymbolToken(char *value, char *type, int lineNumber, int tableno);
+  
+
+#endif // _TABLE_STRUCTURE_H
--- src/Parser/y.tab.h
+++ src/Parser/y.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -45,99 +45,67 @@ extern int yydebug;
 # define YYTOKENTYPE
   enum yytokentype
   {
-    CHAR = 258,
-    INT = 259,
-    FLOAT = 260,
-    DOUBLE = 261,
-    SHORT = 262,
-    UNSIGNED = 263,
-    LONG = 264,
-    RETURN = 265,
+    VOID = 258,
+    CHAR = 259,
+    INT = 260,
+    FLOAT = 261,
+    DOUBLE = 262,
+    SHORT = 263,
+    UNSIGNED = 264,
+    LONG = 265,
     IF = 266,
     ELSE = 267,
     WHILE = 268,
-    L_FLOWER_BRKT = 269,
-    R_FLOWER_BRKT = 270,
-    L_PAREN = 271,
-    R_PAREN = 272,
-    STRING = 273,
-    VOID = 274,
-    BREAK = 275,
-    L_SQR_BRKT = 276,
-    R_SQR_BRKT = 277,
-    MUL_COMMENT_A = 278,
-    MUL_COMMENT_B = 279,
-    SL_COMMENT = 280,
-    IDENTIFIER = 281,
-    NUM_INTEGER = 282,
-    NUM_FLOAT = 283,
-    SEMICOLON = 284,
-    COMMA = 285,
-    REL_LESSEQUAL = 286,
-    REL_GREATEQUAL = 287,
-    REL_EQUAL = 288,
-    REL_NOTEQUAL = 289,
-    REL_LESSTHAN = 290,
-    REL_GREATERTHAN = 291,
-    AR_PLUS = 292,
-    AR_MINUS = 293,
-    AR_MUL = 294,
-    AR_DIV = 295,
-    AR_MOD = 296,
-    BITWISE_XOR = 297,
-    BITWISE_AND = 298,
-    BITWISE_OR = 299,
-    LOG_AND = 300,
-    LOG_OR = 301,
-    LOG_COMPARE = 302
+    RETURN = 269,
+    BREAK = 270,
+    CONTINUE = 271,
+    IDENTIFIER = 272,
+    NUM_INTEGER = 273,
+    NUM_FLOAT = 274,
+    STRING_LITERAL = 275,
+    REL_LESSEQUAL = 276,
+    REL_GREATEQUAL = 277,
+    REL_EQUAL = 278,
+    REL_NOTEQUAL = 279,
+    REL_LESSTHAN = 280,
+    REL_GREATERTHAN = 281,
+    LOG_AND = 282,
+    LOG_OR = 283,
+    LOG_COMPARE = 284,
+    INC_OP = 285,
+    DEC_OP = 286
   };
 #endif
 /* Tokens.  */
-#define CHAR 258
-#define INT 259
-#define FLOAT 260
-#define DOUBLE 261
-#define SHORT 262
-#define UNSIGNED 263
-#define LONG 264
-#define RETURN 265
+#define VOID 258
+#define CHAR 259
+#define INT 260
+#define FLOAT 261
+#define DOUBLE 262
+#define SHORT 263
+#define UNSIGNED 264
+#define LONG 265
 #define IF 266
 #define ELSE 267
 #define WHILE 268
-#define L_FLOWER_BRKT 269
-#define R_FLOWER_BRKT 270
-#define L_PAREN 271
-#define R_PAREN 272
-#define STRING 273
-#define VOID 274
-#define BREAK 275
-#define L_SQR_BRKT 276
-#define R_SQR_BRKT 277
-#define MUL_COMMENT_A 278
-#define MUL_COMMENT_B 279
-#define SL_COMMENT 280
-#define IDENTIFIER 281
-#define NUM_INTEGER 282
-#define NUM_FLOAT 283
-#define SEMICOLON 284
-#define COMMA 285
-#define REL_LESSEQUAL 286
-#define REL_GREATEQUAL 287
-#define REL_EQUAL 288
-#define REL_NOTEQUAL 289
-#define REL_LESSTHAN 290
-#define REL_GREATERTHAN 291
-#define AR_PLUS 292
-#define AR_MINUS 293
-#define AR_MUL 294
-#define AR_DIV 295
-#define AR_MOD 296
-#define BITWISE_XOR 297
-#define BITWISE_AND 298
-#define BITWISE_OR 299
-#define LOG_AND 300
-#define LOG_OR 301
-#define LOG_COMPARE 302
+#define RETURN 269
+#define BREAK 270
+#define CONTINUE 271
+#define IDENTIFIER 272
+#define NUM_INTEGER 273
+#define NUM_FLOAT 274
+#define STRING_LITERAL 275
+#define REL_LESSEQUAL 276
+#define REL_GREATEQUAL 277
+#define REL_EQUAL 278
+#define REL_NOTEQUAL 279
+#define REL_LESSTHAN 280
+#define REL_GREATERTHAN 281
+#define LOG_AND 282
+#define LOG_OR 283
+#define LOG_COMPARE 284
+#define INC_OP 285
+#define DEC_OP 286
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
